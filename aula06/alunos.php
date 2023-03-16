d273ce5f7bf29f678896b6d3660df62eafad0d7c
Miguel de Oliveira Maciel <11134889640@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-16 11:22:53 -0300

    index

---
+++ aula06/alunos.php
@@ -0,0 +1,31 @@
+<pre>
+<?php
+$aluno1 = array(
+    "nome" => "poliana silveira",
+    "nota1" => "7",
+    "nota2" => "9"
+);
+$aluno2 = array(
+    "nome" => "murilo alexandre",
+    "nota1" => "8",
+    "nota2" => "5"
+);
+$aluno3 = array(
+    "nome" => "andre felipe",
+    "nota1" => "6",
+    "nota2" => "7"
+);
+$aluno4 = array(
+    "nome" => "lavinia sandi",
+    "nota1" => "8",
+    "nota2" => "9"
+);    
+
+$dados = [$aluno1, $aluno2, $aluno3, $aluno4];
+
+//print_r($dados);
+
+echo json_encode($dados);
+
+?>
+</pre>
